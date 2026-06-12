46f04d003e16769e3634657bbdf0347b87774cd3
yashsenown3172@gmail.com <yashsenown3172@gmail.com>
2026-06-13 01:24:46 +0530

    chore: add real Google Client ID to environment.ts

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 // src/environments/environment.ts
 export const environment = {
   apiUrl: 'http://localhost:8080',
-  googleClientId: 'YOUR_GOOGLE_CLIENT_ID.apps.googleusercontent.com' // Replace with your actual Google Client ID
+  googleClientId: '986062112102-fa0hr5aon8i43ugubc35ki0mbuofqntn.apps.googleusercontent.com' // Replace with your actual Google Client ID
 };
